a518b887e8bdccd9a6a56df6d80c3d3eb90b7059
EunSeongL <dldmstjd07272@gmail.com>
2025-07-21 09:10:27 +0900

    update

--- FFT/module/BF_ALU/cmul.sv
+++ FFT/module/BF_ALU/cmul.sv
@@ -1,6 +1,7 @@
 module cmul #(
-    parameter BFLY = 10;
-    parameter TW = 9;
+    parameter BFLY = 10,
+    parameter TW = 9,
+    parameter WIDTH = BFLY + TW
 )(
     input signed [BFLY-1:0] bfly_re,
     input signed [BFLY-1:0] bfly_im,
@@ -9,8 +10,6 @@ module cmul #(
     output signed [WIDTH-1:0] out_re,
     output signed [WIDTH-1:0] out_im
 );
-
-    localparam WIDTH = BFLY + TW;
 
     assign out_re = (bfly_re * tw_re) - (bfly_im * tw_im);
     assign out_im = (bfly_im * tw_re) + (bfly_re * tw_im);
