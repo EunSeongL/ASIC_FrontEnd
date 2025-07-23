5b3578835f6295a70ae7dd67be958f78393f173d
EunSeongL <128117825+EunSeongL@users.noreply.github.com>
2025-07-23 22:35:28 +0900

    mem_tw4.sv 업데이트

--- FFT/module/MEM/mem_tw4.sv
+++ FFT/module/MEM/mem_tw4.sv
@@ -1,22 +1,22 @@
 module mem_tw4 (
     input  logic [1:0] addr,        
-    output logic [9:0] dout_re,     
-    output logic [9:0] dout_im      
+    output logic [8:0] dout_re,     
+    output logic [8:0] dout_im      
 );
 
-    logic [19:0] dout;
+    logic [17:0] dout;
     
     always @(*) begin
         case (addr)
-            2'b00: dout = 20'b0_1_0000000_0_0_0000000;
-            2'b01: dout = 20'b0_1_0000000_0_0_0000000;
-            2'b10: dout = 20'b0_1_0000000_0_0_0000000;
-            2'b11: dout = 20'b0_0_0000000_1_1_0000000;
-            default: dout = 20'b0;
+            2'b00: dout = 18'b0_1_0000000_0_0_0000000;
+            2'b01: dout = 18'b0_1_0000000_0_0_0000000;
+            2'b10: dout = 18'b0_1_0000000_0_0_0000000;
+            2'b11: dout = 18'b0_0_0000000_1_1_0000000;
+            default: dout = 18'b0;
         endcase
     end
     
-    assign dout_re = dout[19:10];
-    assign dout_im = dout[9:0];
+    assign dout_re = dout[17:9];
+    assign dout_im = dout[8:0];
 
 endmodule
