3fb903844135c8ea0a6c9bffdfa2478f9317dc11
EunSeongL <dldmstjd07272@gmail.com>
2025-07-23 12:59:29 +0900

    ctrl

--- FFT/module/controller/ctrl.sv
+++ FFT/module/controller/ctrl.sv
@@ -1,6 +1,18 @@
 module ctrl (
+    input clk,
+    input rstn,
     
+    output logic cnt_256,
+    output logic cnt_128,
+    output logic cnt_64
 );
     
-    
+    always_ff @(posedge clk or negedge rstn) begin
+        if(!rstn) begin
+            cnt_64 <= 0;
+            cnt_128 <= 0;
+            cnt_256 <= 0;
+        end
+    end
+
 endmodule
