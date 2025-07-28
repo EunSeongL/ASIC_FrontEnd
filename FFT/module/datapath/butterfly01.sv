1ffee2c3c2a2aa315bb2484c036312734d237e27
EunSeongL <128117825+EunSeongL@users.noreply.github.com>
2025-07-28 09:09:20 +0900

    Update butterfly01.sv

--- FFT/module/datapath/butterfly01.sv
+++ FFT/module/datapath/butterfly01.sv
@@ -4,7 +4,7 @@ module butterfly01 #(
     parameter IN_WIDTH  = 10,          //input bit width
     parameter OUT_WIDTH = 11,         //output bit width
     parameter NUM       = 16,         //number of line
-    parameter DATA      = 512,        //number of data
+    parameter DATA      = 256,        //number of data
     parameter COUNT     = DATA / NUM  //input data count
 ) (
     input clk,
@@ -14,12 +14,14 @@ module butterfly01 #(
     input  signed [IN_WIDTH-1:0]  din_q    [0:NUM-1],  //Im value
     input                         valid_in,           //valid input
 
-    output signed [OUT_WIDTH-1:0] do1_re_bfly01   [0:NUM-1],  //plus Re out
-    output signed [OUT_WIDTH-1:0] do1_im_bfly01   [0:NUM-1],  //plus Im out
-    output signed [OUT_WIDTH-1:0] do2_re_bfly01   [0:NUM-1],  //minus Re out
-    output signed [OUT_WIDTH-1:0] do2_im_bfly01   [0:NUM-1],  //minus Im out
+    output logic signed [OUT_WIDTH-1:0] do1_re  [0:NUM-1],  //plus Re out
+    output logic signed [OUT_WIDTH-1:0] do1_im  [0:NUM-1],  //plus Im out
+    output logic signed [OUT_WIDTH-1:0] do2_re  [0:NUM-1],  //minus Re out
+    output logic signed [OUT_WIDTH-1:0] do2_im  [0:NUM-1],  //minus Im out
     output                        valid_out            //valid output
 );
+
+  
 
     logic [$clog2(COUNT):0] count;  //0~32
     logic shift_reg_cntl_write;
@@ -28,10 +30,10 @@ module butterfly01 #(
     logic signed [IN_WIDTH-1:0] shift_reg_val_im[0:NUM-1];
     logic bfly_en;  //0: 대기, 1: bfly계산
 
-    assign shift_reg_cntl_write = (valid_in && (count<=COUNT/2)) ? 1 : 0;
+    assign shift_reg_cntl_write = (valid_in && (count<=(COUNT/2))) ? 1 : 0;
     assign shift_reg_cntl_read = (count >= (COUNT/2)) ? 1 : 0;
     assign bfly_en = (count >= (COUNT / 2)) ? 1 : 0;
-    assign valid_out = (count > (COUNT / 2)) ? 1 : 0;
+    assign valid_out_bfly01 = (count > (COUNT / 2)) ? 1 : 0;
 
     always @(posedge clk or negedge rstn) begin
         if (!rstn) begin
@@ -82,18 +84,21 @@ module butterfly01 #(
         .data_out_imag(shift_reg_val_im)
     );
 
-    bfly_add #(
+    bfly01 #(
         .INT(4),
         .FLT(6)
-    ) u_bfly_add (
-        .din1_re(din_i),
-        .din1_im(din_q),
-        .din2_re(shift_reg_val_re),
-        .din2_im(shift_reg_val_im),
-        .dout1_re(do1_re_bfly01),
-        .dout1_im(do1_im_bfly01),
-        .dout2_re(do2_re_bfly01),
-        .dout2_im(do2_im_bfly01)
+    ) U_BFLY_SUM_AND_MUL01 (
+        .clk (clk),
+        .rstn(rstn),
+        .bfly_en(bfly_en),
+        .din1_i (low_reg_i),
+        .din1_q (low_reg_q),
+        .din2_i (shift_reg_val_re),
+        .din2_q (shift_reg_val_im),
+        .dout1_i(do1_re),
+        .dout1_q(do1_im),
+        .dout2_i(do2_re),
+        .dout2_q(do2_im)
     );
 
 endmodule
